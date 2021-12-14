249cdc12e63c48d4b8818ef0ea986455a2e00286
Caleb Danielsen <csdigital2022@gmail.com>
2021-12-14 23:43:33 +0100

    Initial Commit

---
+++ InstagramSwiftUI/InstagramSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramSwiftUIApp.swift
+//  InstagramSwiftUI
+//
+//  Created by Caleb Danielsen on 14/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct InstagramSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
