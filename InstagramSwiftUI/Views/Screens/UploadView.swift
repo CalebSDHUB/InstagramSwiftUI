f8c031da18e954a2af345783b9b79ec1c54f6692
Caleb Danielsen <csdigital2022@gmail.com>
2021-12-18 10:36:32 +0100

    upload

--- InstagramSwiftUI/Views/Screens/UploadView.swift
+++ InstagramSwiftUI/Views/Screens/UploadView.swift
@@ -5,14 +5,21 @@
 //  Created by Caleb Danielsen on 18/12/2021.
 //
 
+import UIKit
 import SwiftUI
 
 struct UploadView: View {
+    
+    @State var showImagePicker: Bool = false
+    @State var imageSelected: UIImage = UIImage(named: "logo")!
+    @State var sourceType: UIImagePickerController.SourceType = .camera
+    
     var body: some View {
         ZStack {
             VStack {
                 Button(action: {
-                    
+                    sourceType = UIImagePickerController.SourceType.camera
+                    showImagePicker.toggle()
                 }, label: {
                     Text("Take photo".uppercased())
                         .font(.largeTitle)
@@ -23,7 +30,8 @@ struct UploadView: View {
                 .background(Color.Theme.purpleColor)
                 
                 Button(action: {
-                    
+                    sourceType = UIImagePickerController.SourceType.photoLibrary
+                    showImagePicker.toggle()
                 }, label: {
                     Text("Import photo".uppercased())
                         .font(.largeTitle)
@@ -33,6 +41,10 @@ struct UploadView: View {
                 .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
                 .background(Color.Theme.yellowColor)
             }
+            
+            .sheet(isPresented: $showImagePicker, content: {
+                ImagePicker(imageSelected: $imageSelected, sourceType: $sourceType)
+            })
             
             Image("logo.transparent")
                 .resizable()
